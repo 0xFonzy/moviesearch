1006af9c51cc328588d0794bf59ea4babbe3cd25
0xFonzy <fonzy@fonzy.io>
2024-06-12 03:32:44 -0700

    use next ui navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,55 +1,68 @@
 'use client';
-import { useContext, useState } from 'react';
-import Link from 'next/link';
+import { useContext } from 'react';
 import Image from 'next/image';
 import { SearchContext } from '../context/SearchContext';
+import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input, Navbar as Nav, NavbarBrand, NavbarContent } from '@nextui-org/react';
+import { FaSearch } from 'react-icons/fa';
+import Link from 'next/link';
 
 export default function Navbar() {
   const { query, setQuery, handleSearch } = useContext(SearchContext);
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
-    <nav className="flex items-center justify-between max-w-full mx-auto p-8">
-      <div className="text-2xl font-bold">
-        <Image src="/logo.png" alt="Logo" height={100} width={200} />
-      </div>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          placeholder="Search for a movie..."
-          className="w-72 p-2 border border-gray-300 rounded"
-        />
-        <button type="submit" className="hidden">Search</button>
-      </form>
-      <div className="relative">
-        <div
-          onMouseEnter={() => setIsDropdownOpen(true)}
-          onMouseLeave={() => setIsDropdownOpen(false)}
-        >
-          <Image
-            src="/avatar.png"
-            alt="Profile"
-            height={50}
-            width={50}
-            className="w-10 h-10 rounded-full cursor-pointer"
-          />
-          {isDropdownOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded shadow-lg">
-              <Link href="/profile" className='block px-4 py-2 hover:bg-gray-200'>
-                Profile
-              </Link>
-              <Link href="/settings" className='block px-4 py-2 hover:bg-gray-200'>
-                Settings
-              </Link>
-              <Link href="/logout" className='block px-4 py-2 hover:bg-gray-200'>
-                Logout
-              </Link>
-            </div>
-          )}
-        </div>
-      </div>
-    </nav>
+    <Nav isBlurred maxWidth='full'>
+      <NavbarContent justify='start'>
+        <NavbarBrand className='mr-4'>
+          <Link href='/'>
+            <Image src="/logo.png" alt="Logo" height={100} width={200} />
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent justify='center'>
+        <form onSubmit={handleSearch} className='flex items-center'>
+          <Input classNames={{
+            base: "max-w-full sm:max-w-[10rem] h-10",
+            mainWrapper: "h-full",
+            input: "text-small",
+            inputWrapper: "w-72 p-2 border border-gray-300 rounded",
+          }}
+            placeholder="Search for a movie..."
+            size="sm"
+            startContent={<FaSearch size={18} />}
+            type="search"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)} />
+        </form>
+      </NavbarContent>
+      <NavbarContent as='div' className='items-center' justify='end'>
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              name="Alfonso Aranzazu"
+              size="sm"
+              src="/avatar.png"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">alfonso.aranzazu@gmail.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+    </Nav>
   );
 }
